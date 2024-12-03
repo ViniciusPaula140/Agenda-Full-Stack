087af22a49969229e3cf29354de1ca97c9c639b2
Vinícius Paula <viniciuspaularibeiro02@gmail.com>
2024-12-03 15:37:58 -0300

    Update contatoController.js

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -21,7 +21,7 @@ exports.register = async (req, res) => {
 
         req.flash('success', 'Contato registrado com sucesso!');
         req.session.save(() => {
-            return res.redirect(`/contato/index/${contato.contato._id}`);
+            return res.redirect(`/contato/index`);
         });
     } catch(e) {
         console.log(e);
